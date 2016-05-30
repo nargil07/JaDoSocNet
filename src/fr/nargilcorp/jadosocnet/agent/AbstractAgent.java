2f0731d7cabec2bf81874cd56192d95c02d1d82b
nargil07 <nargilcorp@gmail.com>
2016-05-30 22:07:13 +0200

    first commit

---
+++ src/fr/nargilcorp/jadosocnet/agent/AbstractAgent.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.nargilcorp.jadosocnet.agent;
+
+/**
+ *
+ * @author antony
+ */
+public abstract class AbstractAgent implements AgentInterface, Runnable{
+    
+    private String name;
+
+    public AbstractAgent(){
+        
+    }
+    
+}
